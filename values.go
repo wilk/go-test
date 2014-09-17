fb889e2a916a4fa7c19dd67fea9fdadcdfa8ffba
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-09-17 08:47:18 +0200

    added values example

---
+++ values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hi" + " " + "there")
+
+	fmt.Println("let's print some ints: ", 10 + 10)
+
+	fmt.Println("and what about floats? ", 10.20 - 20.30)
+
+	fmt.Println("Booleans!")
+	fmt.Println(true && false)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+}
